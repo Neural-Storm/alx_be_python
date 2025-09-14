359df33d4bb57ba7a0138fad4661837647763c1c
Galal Ahmed <galalahmed@Galals-MacBook-Air.local>
2025-09-15 01:36:55 +0200

    datetime

--- fns_and_dsa/explore_datetime.py
+++ fns_and_dsa/explore_datetime.py
@@ -1,12 +1,12 @@
-import datetime
+from datetime import datetime , timedelta
 
 def display_current_datetime():
-    current_date = datetime.datetime.now()
+    current_date = datetime.now()
     print(current_date.strftime("%Y-%m-%d %H:%M:%S"))
     return current_date
 
 def calculate_future_date(current_date , days):
-    future_date = current_date + datetime.timedelta(days = days)
+    future_date = current_date + timedelta(days = days)
     print(f"Future date: {future_date}")
     return future_date
 
